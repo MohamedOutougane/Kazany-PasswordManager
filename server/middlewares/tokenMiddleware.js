98de95c6d18d63c28283d5a6db82d5168c936e9d
Outougane Mohamed <outougane.moh@gmail.com>
2023-03-17 10:18:34 +0100

    edit comment

--- server/middlewares/tokenMiddleware.js
+++ server/middlewares/tokenMiddleware.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const secret = process.env.SECRET_JWT;  // get the secret jwt from the .env file
 
-// generate a token with the user id and the secret jwt
+// generate a token with the user id and the secret jwt that expires in 5 minutes
 const generateToken = (id) => {
     return jwt.sign({ id }, secret, {
         expiresIn: 300,
