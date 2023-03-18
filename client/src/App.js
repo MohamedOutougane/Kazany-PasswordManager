dabbb10a622dd0912ea88b81c6748e8a0f0eb949
Outougane Mohamed <outougane.moh@gmail.com>
2023-03-18 22:19:05 +0100

    adding route to dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Header from './components/Header';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
@@ -14,9 +17,11 @@ function App() {
             <Route path='/' element={<Home />} />
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
+            <Route path='/dashboard' element={<Dashboard />} />
           </Routes>
         </div>
       </Router>
+      <ToastContainer />
     </>
   );
 }
